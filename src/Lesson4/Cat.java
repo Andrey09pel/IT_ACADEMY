ad5c827bdc353dd32d82d59c771a19c6e252a142
Andrey <andrei09pel@gmail.com>
2023-07-04 00:28:17 +0300

    Lesson4() не конечный вариант

--- src/Lesson4/Cat.java
+++ src/Lesson4/Cat.java
@@ -1,34 +1,78 @@
 package Lesson4;
 
+
+import java.util.Scanner;
+import java.util.Random;
 public class Cat {
+
     public static String cat_name = "Кот" ;
-    public static int cat_age = 1;
+    public static String cat_age = "1";
     public static String[][] whileCatArr = new String[2][10];
     public  void whileCat() {
         int idx = 0;
         int idy = 0;
-        System.out.println("start");
-        System.out.println("idx "+idx);
-        System.out.println("idy "+idy);
-        while (idx > 9 && idy > 2){
-            System.out.println(idy);
-            System.out.println(idy);
-            if(idy == 0 ){
-                whileCatArr[idx][idy] = cat_name;
-            }
-            if(idy == 1 ){
-                whileCatArr[idx][idy] = String.valueOf(cat_age);
-            }
+
+        while (idx < 10 ){
+            whileCatArr[0][idx] = cat_name;
+            whileCatArr[1][idx] = cat_age;
             idx++;
-            if(idx == 10){
-                idy++;
-                idx=0;
-            }
-
-
         }
     }
 
+    public  void ForCat() {
+        int idx = 0;
+
+        for ( idx = 0; idx < whileCatArr[0].length; idx++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите имя котика ");
+            String NameCat = String.valueOf(scanner.nextLine());
+            setArr(0,idx,NameCat); // установка значения имя
+            System.out.println("Введите возрост котика ");
+            String AgeCat = String.valueOf(scanner.nextLine());
+            setArr(1,idx,AgeCat); // установка значения возраст
+            getArr(idx); // вывод установленных значений
+        }
+    }
+
+    public  void doWhileCat() {
+        int idx = 0;
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите имя котика ");
+            String NameCat = String.valueOf(scanner.nextLine());
+            whileCatArr[0][idx] = NameCat;
+            System.out.println("Введите возрост котика ");
+            String AgeCat = String.valueOf(scanner.nextLine());
+            whileCatArr[1][idx] = AgeCat; // установка значения возраст
+            idx++;
+         } while (idx < whileCatArr[0].length);
+    }
+
+    public  void forEachCat( ) {
+        int idx = 0;
+        int idy= 0;
+        int random = 0;
+        int n = 5;
+        //String[][] whileCatArr
+        for (String[] k :whileCatArr){
+            for(String i: k){
+                for (int z = 1; z < 28; z++) {
+                    n= generateRandomNumber(n);
+                }
+
+                whileCatArr[0][idx] = String.valueOf(n) ;
+                whileCatArr[1][idx] = String.valueOf(n) ;
+            }
+        }
+    }
+
+    public void  getArr(int idx) {
+        System.out.print("Имя котика - "+whileCatArr[0][idx] + " возрост "+whileCatArr[1][idx]+"\t");
+    }
+    public void setArr(int idy,int idx,String valye) {
+        this.whileCatArr[idy][idx] = valye;
+    }
     public  void getArrCat() {
         for (int i = 0; i < whileCatArr.length; i++) {
             for (int j = 0; j < whileCatArr[i].length; j++) {
@@ -36,6 +80,24 @@ public class Cat {
             }
             System.out.println();
         }
-
     }
+
+    public  void clearArrCat() {
+        for (int i = 0; i < whileCatArr.length; i++) {
+            for (int j = 0; j < whileCatArr[i].length; j++) {
+                whileCatArr[i][j] = null;
+            }
+        }
+    }
+    public static int generateRandomNumber(int n)
+    {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must not be negative");
+        }
+
+        // генерируем случайное число от 0 до `n`
+        return new Random().nextInt(n + 1);
+    }
+
+
 }
--- src/Lesson4/Main.java
+++ src/Lesson4/Main.java
@@ -1,9 +1,28 @@
 package Lesson4;
 
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
+
     public static void main(String[] args) {
         Cat cat = new Cat();
+        // цикл while
         cat.whileCat();
         cat.getArrCat();
+
+        // цикл For
+        cat.clearArrCat(); // чистим массив
+        cat.ForCat(); // заполняем массив
+        cat.getArrCat(); // вывод всего массива
+
+        // цикл do while
+        cat.clearArrCat(); // чистим массив
+        cat.doWhileCat();
+        cat.getArrCat(); // вывод всего массива
+
+        // цикл foreach
+        cat.clearArrCat(); // чистим массив
+        cat.forEachCat();
+        cat.getArrCat(); // вывод всего массива
     }
 }
